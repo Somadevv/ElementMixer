cdfe4d0d99ff8435a6af3818f58cbef486172c7d
josh howarth <joshi@joshs-MacBook-Pro.local>
2021-12-13 16:11:10 +0000

    File structuring & responsive imaging

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,7 +2,7 @@ const desktopBackground = document.querySelector('.home-container');
 const mobileBackground = document.querySelector('.toggle-mobile');
 const mobileQuery = window.matchMedia("(max-width: 768px)");
 const backGeneratoe = document.qu
-// const inventoryItem = `<img src="/static/images/inventory_slot.png" id='inventorySlot' alt="inventory item">`;
+
 
 const checkQuery = (query) => {
   query.matches
