29cefa98f29d43ed012f816539d44be1c757d235
Josh Howarth <75903665+Somadevv@users.noreply.github.com>
2022-01-05 09:18:25 +0000

    Delete env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-os.environ["NAME"] = "ElementMixerDB"
-os.environ["USER"] = "postgres"
-os.environ["PASSWORD"] = "R3zimatazz8"
-os.environ["HOST"] = "localhost"
-os.environ["PORT"] = "8000"
