a77498790d1fe03ebe809f66c805853c4646b1d3
Josh <somadev@hotmail.co.uk>
2022-01-14 10:40:43 +0000

    Code refactor

--- static/scripts/Components/toggleViews.js
+++ static/scripts/Components/toggleViews.js
@@ -1,55 +1,56 @@
 import { drawElements } from "./elements.js";
 
-// Ugly code, I know, I'm sorry!
-const ToggleViews = {
-  // DOM Element selectors
-  cachedElements: {
-    cauldronContainer: document.getElementById("cauldron-container"),
-    shopContainer: document.getElementById("shop-container"),
-    elementsContainer: document.getElementById("elements-container"),
-    toggleCauldronView: document.getElementById("toggleCauldron"),
-    toggleElementsView: document.getElementById("toggleElements"),
-    toggleShopView: document.getElementById("toggleShop"),
-    shopSlots: document.querySelectorAll(".shop-slot"),
-  },
-  toggleView: (view) => {
-    // Switch case to trigger each event
-    switch (view) {
-      case "cauldron":
-        ToggleViews.cachedElements.shopContainer.classList.add("disabled");
-        ToggleViews.cachedElements.elementsContainer.classList.add("disabled");
-        ToggleViews.cachedElements.cauldronContainer.classList.remove("disabled");
-        break;
-      case "tiers":
-        ToggleViews.cachedElements.shopContainer.classList.add("disabled");
-        ToggleViews.cachedElements.cauldronContainer.classList.add("disabled");
-        ToggleViews.cachedElements.elementsContainer.classList.remove("disabled");
-        break;
-      case "shop":
-        ToggleViews.cachedElements.cauldronContainer.classList.add("disabled");
-        ToggleViews.cachedElements.elementsContainer.classList.add("disabled");
-        ToggleViews.cachedElements.shopContainer.classList.remove("disabled");
-        break;
-    }
-  },
+// Repeated code, i know, i'm sorry!
+
+// DOM Element selectors
+const cachedEls = {
+  cauldronContainer: document.getElementById("cauldron-container"),
+  shopContainer: document.getElementById("shop-container"),
+  elementsContainer: document.getElementById("elements-container"),
+  toggleCauldron: document.getElementById("toggleCauldron"),
+  toggleElements: document.getElementById("toggleElements"),
+  toggleShop: document.getElementById("toggleShop"),
+  shopSlots: document.querySelectorAll(".shop-slot"),
 };
+
+// Switch case to trigger each event
+const toggleView = (view) => {
+  switch (view) {
+    case "cauldron":
+      cachedEls.shopContainer.classList.add("disabled");
+      cachedEls.elementsContainer.classList.add("disabled");
+      cachedEls.cauldronContainer.classList.remove("disabled");
+      break;
+    case "tiers":
+      cachedEls.shopContainer.classList.add("disabled");
+      cachedEls.cauldronContainer.classList.add("disabled");
+      cachedEls.elementsContainer.classList.remove("disabled");
+      break;
+    case "shop":
+      cachedEls.cauldronContainer.classList.add("disabled");
+      cachedEls.elementsContainer.classList.add("disabled");
+      cachedEls.shopContainer.classList.remove("disabled");
+      break;
+  }
+};
+
 // toggleView() usage(s)
-ToggleViews.cachedElements.toggleShopView.addEventListener("click", () => {
-  ToggleViews.cachedElements.toggleShopView.classList.add("active");
-  ToggleViews.cachedElements.toggleElementsView.classList.remove("active");
-  ToggleViews.cachedElements.toggleCauldronView.classList.remove("active");
-  ToggleViews.toggleView("shop");
+cachedEls.toggleShop.addEventListener("click", () => {
+  cachedEls.toggleShop.classList.add("active");
+  cachedEls.toggleElements.classList.remove("active");
+  cachedEls.toggleCauldron.classList.remove("active");
+  toggleView("shop");
 });
-ToggleViews.cachedElements.toggleCauldronView.addEventListener("click", () => {
-  ToggleViews.cachedElements.toggleCauldronView.classList.add("active");
-  ToggleViews.cachedElements.toggleShopView.classList.remove("active");
-  ToggleViews.cachedElements.toggleElementsView.classList.remove("active");
-  ToggleViews.toggleView("cauldron");
+cachedEls.toggleCauldron.addEventListener("click", () => {
+  cachedEls.toggleCauldron.classList.add("active");
+  cachedEls.toggleShop.classList.remove("active");
+  cachedEls.toggleElements.classList.remove("active");
+  toggleView("cauldron");
 });
-ToggleViews.cachedElements.toggleElementsView.addEventListener("click", () => {
-  drawElements()
-  ToggleViews.cachedElements.toggleCauldronView.classList.remove("active");
-  ToggleViews.cachedElements.toggleShopView.classList.remove("active");
-  ToggleViews.cachedElements.toggleElementsView.classList.add("active");
-  ToggleViews.toggleView("tiers");
+cachedEls.toggleElements.addEventListener("click", () => {
+  drawElements();
+  cachedEls.toggleCauldron.classList.remove("active");
+  cachedEls.toggleShop.classList.remove("active");
+  cachedEls.toggleElements.classList.add("active");
+  toggleView("tiers");
 });
